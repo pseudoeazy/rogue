b073ab64fa15dbf9f953d0c5aa0a50fd9b6fe6e0
israel <easy4sng@gmail.com>
2023-09-01 13:22:09 +0800

    march landing page to required task

--- js/script.js
+++ js/script.js
@@ -2,6 +2,11 @@ const view = {
   checkBox: document.getElementById("nav-toggle"),
   headerRight: document.getElementById("header-right"),
   form: document.getElementById("contact-form"),
+  popup: document.getElementById("popup"),
+  popupClose: document.getElementById("popup-close"),
+  books: document.querySelectorAll(".book"),
+  loader: document.getElementById("loader"),
+
   watch() {
     this.checkBox.addEventListener("click", function () {
       view.headerRight.classList.toggle("slide-in");
@@ -9,6 +14,23 @@ const view = {
 
     this.form.addEventListener("submit", function (e) {
       e.preventDefault();
+      view.loader.classList.add("loader");
+      setTimeout(() => {
+        view.loader.classList.remove("loader");
+        view.form.parentElement.innerHTML = `<div class="response">
+        <span> Thank you for your email <br />We will get in touch</span>
+        </div>`;
+      }, 3000);
+    });
+
+    [...this.books].forEach((book) => {
+      book.addEventListener("click", () => {
+        view.popup.style.display = "block";
+      });
+    });
+
+    this.popupClose.addEventListener("click", () => {
+      view.popup.style.display = "none";
     });
   },
 };
